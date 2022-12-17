2b97ad66c083dc652e757ffe3fb37bed56c29cf3
JuampiAcosta <acostajuan678@gmail.com>
2022-12-17 20:00:23 -0300

    Cambios en git-ignore y en gif.js

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -2,12 +2,13 @@ import React,{component} from "react";
 import { render } from 'react-dom'
 import{BrowserRouter, Link,Route,Switch} from 'react-router-dom'
 class Gif extends component{
-constructor(props){super(props) this.state={gif:""}}
+constructor(props){super(props); this.state={gif:""}}
 componentDidMount(){
 fetch("https://api.giphy.com/v1/gifs/random?api_key=FSHnaiOlYd2NXPdn06Qdh64qxmGRVM69&tag=&rating=g")
 .then(results=>results.json())
 .then(data=>data.image_url)
-.catch(error=>error)
+.catch(error=>console.log(error)
+  )
 }    
 render(){
   let contenido
